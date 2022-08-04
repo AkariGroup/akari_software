b130975679f1021e979084c2b5dda3fef0ae9287
Yuki Igarashi <me@bonprosoft.com>
2022-08-04 23:41:52 +0900

    Always recreate uid/gid in daemon entrypoint (#28)

--- internal/docker/daemon-entrypoint.sh
+++ internal/docker/daemon-entrypoint.sh
@@ -8,6 +8,32 @@ DOCKER_GROUP_ID=${DOCKER_GID:-9002}
 
 USERNAME=user
 
+get_entry() {
+  ENTRY=$(getent $1 $2 | cut -d':' -f1)
+  echo "$ENTRY"
+}
+
+remove_group_if_exists() {
+  ENTRY=$(get_entry group $1)
+  if [ ! -z "${ENTRY}" ]; then
+    echo "Group: $ENTRY exists!"
+    groupdel -f $ENTRY
+  fi
+}
+
+remove_user_if_exists() {
+  ENTRY=$(get_entry passwd $1)
+  if [ ! -z "${ENTRY}" ]; then
+    echo "User: $ENTRY exists!"
+    userdel -f $ENTRY
+  fi
+}
+
+# Remove existing entries
+echo $(remove_user_if_exists $USER_ID)
+echo $(remove_group_if_exists $GROUP_ID)
+echo $(remove_group_if_exists $DOCKER_GROUP_ID)
+
 # Create entries
 getent passwd $USERNAME || useradd -u $USER_ID $USERNAME
 getent group $USERNAME || groupadd -g $GROUP_ID $USERNAME
@@ -17,7 +43,7 @@ getent group docker || groupadd -g $DOCKER_GROUP_ID docker
 groupmod -g $GROUP_ID $USERNAME
 groupmod -g $DOCKER_GROUP_ID docker
 
-# Set user entry
+# Update user entry
 usermod -g $GROUP_ID -u $USER_ID -G $USERNAME,docker $USERNAME
 
 exec /usr/bin/gosu $USERNAME "$@"
