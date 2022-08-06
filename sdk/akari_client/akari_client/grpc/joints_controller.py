93c8126d214dc81d9cffd7570c759a299c007fd2
Yuki Igarashi <me@bonprosoft.com>
2022-08-07 01:49:56 +0900

    Implement grpc client

---
+++ sdk/akari_client/akari_client/grpc/joints_controller.py
@@ -0,0 +1,88 @@
+from typing import List
+
+import grpc
+from akari_proto import joints_controller_pb2
+from akari_proto.grpc.error import deserialize_error
+from akari_proto.joints_controller_pb2_grpc import JointsControllerServiceStub
+from google.protobuf.empty_pb2 import Empty
+
+from ..joint_controller import RevoluteJointController
+from ._error import serializer
+
+
+class _GrpcJointsController:
+    def __init__(self, channel: grpc.Channel) -> None:
+        self._stub = JointsControllerServiceStub(channel)
+
+    @property
+    def stub(self) -> JointsControllerServiceStub:
+        return self._stub
+
+    @deserialize_error(serializer)
+    def get_joint_names(self) -> List[str]:
+        res: joints_controller_pb2.GetJointNamesResponse = self._stub.GetJointNames(
+            Empty()
+        )
+        return list(res.joint_names)
+
+
+class GrpcJointController(RevoluteJointController):
+    def __init__(self, joint_name: str, client: _GrpcJointsController) -> None:
+        self._joint_name = joint_name
+        self._client = client
+
+    @property
+    def joint_name(self) -> str:
+        return self._joint_name
+
+    @property
+    def joint_specifier(self) -> joints_controller_pb2.JointSpecifier:
+        return joints_controller_pb2.JointSpecifier(
+            joint_name=self._joint_name,
+        )
+
+    @deserialize_error(serializer)
+    def get_servo_enabled(self) -> bool:
+        res: joints_controller_pb2.GetServoEnabledResponse = (
+            self._client.stub.GetServoEnabled(self.joint_specifier)
+        )
+        return res.enabled
+
+    @deserialize_error(serializer)
+    def set_servo_enabled(self, enabled: bool) -> None:
+        request = joints_controller_pb2.SetServoEnabledRequest(
+            target_joint=self.joint_specifier,
+            enabled=enabled,
+        )
+        self._client.stub.SetServoEnabled(request)
+
+    @deserialize_error(serializer)
+    def set_profile_acceleration(self, rad_per_sec2: float) -> None:
+        request = joints_controller_pb2.SetProfileAccelerationRequest(
+            target_joint=self.joint_specifier,
+            rad_per_sec2=rad_per_sec2,
+        )
+        self._client.stub.SetProfileAcceleration(request)
+
+    @deserialize_error(serializer)
+    def set_profile_velocity(self, rad_per_sec: float) -> None:
+        request = joints_controller_pb2.SetProfileVelocityRequest(
+            target_joint=self.joint_specifier,
+            rad_per_sec=rad_per_sec,
+        )
+        self._client.stub.SetProfileVelocity(request)
+
+    @deserialize_error(serializer)
+    def set_goal_position(self, rad: float) -> None:
+        request = joints_controller_pb2.SetGoalPositionRequest(
+            target_joint=self.joint_specifier,
+            rad=rad,
+        )
+        self._client.stub.SetGoalPosition(request)
+
+    @deserialize_error(serializer)
+    def get_present_position(self) -> float:
+        res: joints_controller_pb2.GetPresentPositionResponse = (
+            self._client.stub.GetPresentPosition(self.joint_specifier)
+        )
+        return res.rad
---
+++ sdk/akari_client/akari_client/grpc/m5stack.py
@@ -0,0 +1,154 @@
+import json
+from typing import Dict, Iterator, Optional, cast
+
+import grpc
+from akari_client.color import Color
+from akari_proto import m5stack_pb2
+from akari_proto.grpc.error import deserialize_error
+from akari_proto.m5stack_pb2_grpc import M5StackServiceStub
+from google.protobuf.empty_pb2 import Empty
+
+from ..m5stack_client import M5ComDict, M5StackClient
+from ._error import serializer
+
+
+def _as_proto_color(color: Optional[Color]) -> m5stack_pb2.Color:
+    if color is None:
+        return m5stack_pb2.Color(red=-1, green=-1, blue=-1)
+
+    return m5stack_pb2.Color(
+        red=color.red,
+        green=color.green,
+        blue=color.blue,
+    )
+
+
+class GrpcM5StackClient(M5StackClient):
+    def __init__(self, channel: grpc.Channel) -> None:
+        self._stub = M5StackServiceStub(channel)
+
+    @deserialize_error(serializer)
+    def set_dout(self, pin_id: int, value: bool, sync: bool = True) -> None:
+        binary_pins: Dict[str, bool] = {}
+        if pin_id == 0:
+            binary_pins["dout0"] = value
+        elif pin_id == 1:
+            binary_pins["dout1"] = value
+        else:
+            raise ValueError(f"Out of range pin_id: {pin_id}")
+
+        request = m5stack_pb2.SetPinOutRequest(
+            binary_pins=binary_pins,
+            sync=sync,
+        )
+        self._stub.SetPinOut(request)
+
+    @deserialize_error(serializer)
+    def set_pwmout(self, pin_id: int, value: int, sync: bool = True) -> None:
+        int_pins: Dict[str, int] = {}
+        if pin_id == 0:
+            int_pins["pwmout0"] = value
+        else:
+            raise ValueError(f"Out of range pin_id: {pin_id}")
+
+        request = m5stack_pb2.SetPinOutRequest(
+            int_pins=int_pins,
+            sync=sync,
+        )
+        self._stub.SetPinOut(request)
+
+    @deserialize_error(serializer)
+    def set_allout(
+        self,
+        *,
+        dout0: bool,
+        dout1: bool,
+        pwmout0: int,
+        sync: bool = True,
+    ) -> None:
+        binary_pins: Dict[str, bool] = {
+            "dout0": dout0,
+            "dout1": dout1,
+        }
+        int_pins: Dict[str, int] = {
+            "pwmout0": pwmout0,
+        }
+        request = m5stack_pb2.SetPinOutRequest(
+            binary_pins=binary_pins,
+            int_pins=int_pins,
+            sync=sync,
+        )
+        self._stub.SetPinOut(request)
+
+    @deserialize_error(serializer)
+    def reset_allout(self, sync: bool = True) -> None:
+        self._stub.ResetPinOut(m5stack_pb2.ResetPinOutRequest(sync=sync))
+
+    @deserialize_error(serializer)
+    def set_display_color(self, color: Color, sync: bool = True) -> None:
+        request = m5stack_pb2.SetDisplayColorRequest(
+            color=_as_proto_color(color),
+            sync=sync,
+        )
+        self._stub.SetDisplayColor(request)
+
+    @deserialize_error(serializer)
+    def set_display_text(
+        self,
+        text: str,
+        pos_x: int,
+        pos_y: int,
+        size: int,
+        text_color: Optional[Color] = None,
+        back_color: Optional[Color] = None,
+        refresh: bool = False,
+        sync: bool = True,
+    ) -> None:
+        request = m5stack_pb2.SetDisplayTextRequest(
+            text=text,
+            pos_x=pos_x,
+            pos_y=pos_y,
+            size=size,
+            text_color=_as_proto_color(text_color),
+            bg_color=_as_proto_color(back_color),
+            refresh=refresh,
+            sync=sync,
+        )
+        self._stub.SetDisplayText(request)
+
+    @deserialize_error(serializer)
+    def set_display_image(
+        self, filepath: str, pos_x: int, pos_y: int, scale: float, sync: bool = True
+    ) -> None:
+        request = m5stack_pb2.SetDisplayImageRequest(
+            path=filepath,
+            pos_x=pos_x,
+            pos_y=pos_y,
+            scale=scale,
+            sync=sync,
+        )
+        self._stub.SetDisplayImage(request)
+
+    @deserialize_error(serializer)
+    def use_japanese_font(self, enabled: bool, sync: bool = True) -> None:
+        request = m5stack_pb2.UseJapaneseFontRequest(
+            enabled=enabled,
+            sync=sync,
+        )
+        self._stub.UseJapaneseFont(request)
+
+    @deserialize_error(serializer)
+    def reset_m5(self) -> None:
+        self._stub.Reset(Empty())
+
+    @deserialize_error(serializer)
+    def get(self) -> M5ComDict:
+        data: m5stack_pb2.M5StackStatus = self._stub.Get(Empty())
+        return cast(M5ComDict, json.loads(data.status_json))
+
+    @deserialize_error(serializer)
+    def get_stream(self) -> Iterator[M5ComDict]:
+        stream = self._stub.GetStream(Empty())
+        for r in stream:
+            data: m5stack_pb2.M5StackStatus = r
+            yield cast(M5ComDict, json.loads(data.status_json))
