a64a36c123e6706a35be3d48f9fe6758e91d1226
Yuki Igarashi <me@bonprosoft.com>
2022-08-09 02:40:04 +0900

    Add /api prefix to grpc gateway

--- internal/akira/cmd/gateway/main.go
+++ internal/akira/cmd/gateway/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/AkariGroup/akari_software/internal/akira/proto"
+	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -16,24 +17,36 @@ const (
 	GATEWAY_PORT         = ":8080"
 )
 
-func main() {
+func createGrpcGateway() (*runtime.ServeMux, error) {
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
 	if err := proto.RegisterSystemServiceHandlerFromEndpoint(context.Background(), mux, DAEMON_GRPC_ENDPOINT, opts); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := proto.RegisterProjectServiceHandlerFromEndpoint(context.Background(), mux, DAEMON_GRPC_ENDPOINT, opts); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := proto.RegisterAkariServiceServiceHandlerFromEndpoint(context.Background(), mux, DAEMON_GRPC_ENDPOINT, opts); err != nil {
+		return nil, err
+	}
+
+	return mux, nil
+}
+
+func main() {
+	grpcMux, err := createGrpcGateway()
+	if err != nil {
 		panic(err)
 	}
 
+	r := mux.NewRouter()
+	r.PathPrefix("/api").Handler(http.StripPrefix("/api", grpcMux))
+
 	fmt.Printf("Gateway Started at %s\n", GATEWAY_PORT)
-	if err := http.ListenAndServe(GATEWAY_PORT, mux); err != nil {
+	if err := http.ListenAndServe(GATEWAY_PORT, r); err != nil {
 		panic(err)
 	}
 }
