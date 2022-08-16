5d564910ce331c08e074c2975cf74d8dabdb8b69
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2022-08-16 20:08:17 +0900

    Create list type project page

---
+++ internal/akira_frontend/src/components/ProjectList/index.tsx
@@ -0,0 +1,45 @@
+import {
+  TableCell,
+  TableRow,
+  IconButton,
+} from "@mui/material";
+import { Akira_protoProject } from "../../api/@types";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import DeleteIcon from "@mui/icons-material/Delete";
+import LaunchIcon from "@mui/icons-material/Launch";
+import { Link,useNavigate } from "react-router-dom";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+
+type Prop = {
+  project: Akira_protoProject;
+};
+
+export function ProjectList({ project }: Prop) {
+
+  const navigate = useNavigate();
+  const projectPage = () => {
+    navigate(`/projects/details?id=${project.id}`)
+  };
+
+  return (
+    <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 }}} style={{ height: 20 }}>
+      <TableCell sx={{ width: 300 }} > <div onClick={projectPage}>{project.manifest?.name}</div> </TableCell>
+      <TableCell sx={{ width: 300 }}> {project.manifest?.author}</TableCell>
+      <TableCell sx={{ width: 600 }}> {project.manifest?.description}</TableCell>
+      <TableCell><IconButton disabled>
+        <FavoriteIcon />
+      </IconButton>
+        <IconButton component={Link} to="/services">
+          <LaunchIcon />
+        </IconButton>
+        <IconButton disabled>
+          <DeleteIcon />
+        </IconButton>
+        <IconButton sx={{ marginLeft: "auto" }}>
+          <MoreVertIcon />
+        </IconButton>
+      </TableCell>
+    </TableRow>
+  );
+}
--- internal/akira_frontend/src/pages/Projects/index.tsx
+++ internal/akira_frontend/src/pages/Projects/index.tsx
@@ -1,22 +1,106 @@
 import useAspidaSWR from "@aspida/swr";
-import { Stack } from "@mui/material";
+import { GridView, } from "@mui/icons-material";
+import {
+  Stack, Container, Paper, Tab, Tabs,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Box,
+  Button,
+} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import { ReactNode, useState, } from "react";
+import { LayoutRouteProps, Link } from "react-router-dom";
 import { NewProjectButtonCard, ProjectCard } from "../../components/ProjectCard";
+import { ProjectList } from "../../components/ProjectList";
 import { useApiClient } from "../../hooks/api";
 
+/*function LayoutView(props: LayoutRouteProps) {
+  const layout = props;
+  return (
+    layout === 'gridView' ? <GridView /> : <ListView />
+  )
+}
+
+function SwitchBetweenLayout() {
+  const [layout, setLayout] = useState('listView');
+
+  const onClickButton = (layoutInfo: string) => {
+    const currentLayout = layoutInfo; // gets 'listView or gridView
+    setLayout(currentLayout); // only set if state is changed.
+  }
+
+  return (
+    //HTML for 2 buttons with click handler
+    <LayoutView layout={layout} />
+
+  )
+}*/
+
+function Header() {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell sx={{ width: 300 }}>プロジェクト名</TableCell>
+        <TableCell sx={{ width: 300 }}>作者名</TableCell>
+        <TableCell sx={{ width: 600 }}>概要</TableCell>
+        <TableCell ></TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
 export function Projects() {
+  const [mode, setMode] = useState(0);
   const client = useApiClient();
+
   const { data, error } = useAspidaSWR(client?.projects, { enabled: !!client });
 
   if (!data || error) {
     return <></>;
   }
 
+  const onClickButton = (layoutInfo: number) => {
+    const currentLayout = layoutInfo; // gets 'listView or gridView
+    //setLayout(currentLayout); // only set if state is changed.
+  }
+
+
   return (
-    <Stack spacing={2} sx={{ margin: 1 }} direction="row">
+    <Container maxWidth="xl">
+        <Stack spacing={2} sx={{ margin: 1 }} direction="row" >
+          <Tabs value={mode}>
+            <Tab label="Card" value={0} onClick={() => {
+              setMode(0);
+            }} />
+            <Tab label="Table" value={1} onClick={() => {
+              setMode(1);
+            }} />
+          </Tabs>
+      </Stack>
+      <Box>
+        <Button
+          variant="contained"
+          startIcon={<AddIcon />}
+          color="success"
+          component={Link} to="/projects/create"
+        >
+          新規プロジェクト
+        </Button>
+      </Box>
+      <Header />
+        {data.projects?.map((p) => (
+          <ProjectList key={p.id} project={p} />
+        ))}
+    </Container>
+
+    /*<Stack spacing={2} sx={{ margin: 1 }} direction="row">
         <NewProjectButtonCard />
       {data.projects?.map((p) => (
         <ProjectCard key={p.id} project={p} />
       ))}
-    </Stack>
+    </Stack>*/
   );
 }
