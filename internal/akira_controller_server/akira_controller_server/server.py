62aec8fe8b01893529055b8ec8c5812d5189a1f4
Yuki Igarashi <me@bonprosoft.com>
2022-11-06 04:27:47 +0900

    Add CORS middleware

--- internal/akira_controller_server/akira_controller_server/server.py
+++ internal/akira_controller_server/akira_controller_server/server.py
@@ -1,9 +1,17 @@
 import uvicorn
 from akira_controller_server.api import api
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(api)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def serve(host: str, port: int) -> None:
