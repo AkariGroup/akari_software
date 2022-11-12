3387dd7fb45ff9ecdf6d9deeb9cfcfae40f934fe
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2022-11-12 18:35:53 +0900

    fix motor control interface

--- internal/akira_frontend/src/pages/Controller/motor.tsx
+++ internal/akira_frontend/src/pages/Controller/motor.tsx
@@ -24,7 +24,9 @@ const JOYSTICK_CONTROL_MS = 1000 / JOYSTICK_CONTROL_HZ;
 
 // max speed is 100 deg/s
 const JOYSTICK_GAIN = (100 / JOYSTICK_CONTROL_HZ) * DEG2RAD;
+const MIN_VEL = 10;
 const MAX_VEL = 1800;
+const MIN_ACC = 1;
 const MAX_ACC = 10;
 
 type JointControlProps = {
@@ -106,12 +108,13 @@ export function MotorPanel({ controllerClient }: Props) {
   );
   const setBusy = useSetBackdropValue();
   const servoEnabled = servoStatus?.enabled ?? false;
-  const pan_min = servoStatus?.pan_min ?? -2.355;
-  const pan_max = servoStatus?.pan_max ?? 2.355;
-  const tilt_min = servoStatus?.tilt_min ?? -0.91;
-  const tilt_max = servoStatus?.tilt_max ?? 0.91;
-  const [vel, setVel] = useState<number>(servoStatus?.vel ?? 1000);
-  const [acc, setAcc] = useState<number>(servoStatus?.acc ?? 5);
+  const [initialFetch, setInitialFetch] = useState<boolean>(false);
+  const pan_min = servoStatus?.pan_min ?? -1.047;
+  const pan_max = servoStatus?.pan_max ?? 1.047;
+  const tilt_min = servoStatus?.tilt_min ?? -0.576;
+  const tilt_max = servoStatus?.tilt_max ?? 0.576;
+  const [vel, setVel] = useState<number>(0);
+  const [acc, setAcc] = useState<number>(0);
 
   const toggleServo = async (enabled: boolean) => {
     setBusy(true);
@@ -154,9 +157,33 @@ export function MotorPanel({ controllerClient }: Props) {
       body: { pan: positionData.pan + relX, tilt: positionData.tilt + relY },
     });
   };
+  const sendVel = async (val: number) => {
+    val = Math.floor(val);
+    setVel(val);
+    controllerClient.motor.velocity.post({
+      query: { vel: val * DEG2RAD },
+    });
+  };
+  const sendAcc = async (val: number) => {
+    val = Math.floor(val);
+    setAcc(val);
+    controllerClient.motor.acceleration.post({
+      query: { acc: acc * DEG2RAD },
+    });
+  };
+
+  function CheckInitialFetch() {
+    if (!initialFetch && servoStatus) {
+      setVel(Math.floor(servoStatus.vel * RAD2DEG));
+      setAcc(Math.floor(servoStatus.acc * RAD2DEG));
+      setInitialFetch(true);
+    }
+    return <></>;
+  }
 
   return (
     <Box>
+      <CheckInitialFetch />
       <Typography variant="h6">Joint</Typography>
       <Box
         sx={{
@@ -166,90 +193,88 @@ export function MotorPanel({ controllerClient }: Props) {
           marginBottom: 1,
         }}
       >
-        <FormControlLabel
-          control={
-            <Switch
-              checked={servoEnabled}
-              onChange={(_, v) => toggleServo(v)}
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={servoEnabled}
+                  onChange={(_, v) => toggleServo(v)}
+                />
+              }
+              label="Servo"
+              value={!servoEnabled}
             />
-          }
-          label="Servo"
-          value={!servoEnabled}
-        />
-        <Typography>&nbsp;Vel&nbsp;</Typography>
-        <Slider
-          style={{ width: "15%" }}
-          sx={{ flexGrow: 1, marginRight: 2 }}
-          value={vel}
-          min={1}
-          max={MAX_VEL}
-          onChange={(_, v) => {
-            const nv = v as number;
-            setVel(nv);
-            controllerClient.motor.velocity.post({
-              query: { vel: vel * DEG2RAD },
-            });
-          }}
-        />
-        <TextField
-          style={{ width: "15%" }}
-          value={vel}
-          size="small"
-          variant="standard"
-          type="number"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">deg/s</InputAdornment>,
-            inputProps: {
-              style: { textAlign: "right" },
-            },
-          }}
-          onChange={(e) => {
-            if (e.target) {
-              setVel(+e.target.value);
-              controllerClient.motor.velocity.post({
-                query: { vel: +e.target.value * DEG2RAD },
-              });
-            }
-          }}
-        />
-        <Typography>&nbsp;&nbsp;Acc&nbsp;</Typography>
-        <Slider
-          style={{ width: "15%" }}
-          sx={{ flexGrow: 1, marginRight: 2 }}
-          value={acc}
-          min={1}
-          max={MAX_ACC}
-          onChange={(_, v) => {
-            const nv = v as number;
-            setAcc(nv);
-            controllerClient.motor.acceleration.post({
-              query: { acc: acc * DEG2RAD },
-            });
-          }}
-        />
-        <TextField
-          style={{ width: "15%" }}
-          value={acc}
-          size="small"
-          variant="standard"
-          type="number"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">deg/s2</InputAdornment>
-            ),
-            inputProps: {
-              style: { textAlign: "right" },
-            },
-          }}
-          onChange={(e) => {
-            if (e.target) {
-              setAcc(+e.target.value);
-              controllerClient.motor.acceleration.post({
-                query: { acc: +e.target.value * DEG2RAD },
-              });
-            }
-          }}
-        />
+          </Grid>
+          <Grid item xs={6} lg>
+            <Typography>&nbsp;Vel&nbsp;</Typography>
+            <Slider
+              sx={{ flexGrow: 1, marginRight: 2 }}
+              value={vel}
+              min={MIN_VEL}
+              max={MAX_VEL}
+              onChange={(_, v) => {
+                const nv = v as number;
+                sendVel(nv);
+              }}
+            />
+            <TextField
+              value={vel}
+              disabled={!servoEnabled}
+              size="small"
+              variant="standard"
+              type="number"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">deg/s</InputAdornment>
+                ),
+                inputProps: {
+                  style: { textAlign: "right" },
+                  step: "1",
+                },
+              }}
+              onChange={(e) => {
+                if (e.target) {
+                  sendVel(+e.target.value);
+                }
+              }}
+            />
+          </Grid>
+          <Grid item xs={6} lg>
+            <Typography>&nbsp;&nbsp;Acc&nbsp;</Typography>
+            <Slider
+              sx={{ flexGrow: 1, marginRight: 2 }}
+              value={acc}
+              min={MIN_ACC}
+              max={MAX_ACC}
+              onChange={(_, v) => {
+                const nv = v as number;
+                sendAcc(nv);
+              }}
+            />
+            <TextField
+              value={acc}
+              disabled={!servoEnabled}
+              size="small"
+              variant="standard"
+              type="number"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">deg/s2</InputAdornment>
+                ),
+                inputProps: {
+                  style: { textAlign: "right" },
+                  step: "1",
+                },
+              }}
+              onChange={(e) => {
+                if (e.target) {
+                  sendAcc(+e.target.value);
+                }
+              }}
+            />
+          </Grid>
+        </Grid>
       </Box>
       <Grid container spacing={3}>
         <Grid item xs={12} lg="auto">
