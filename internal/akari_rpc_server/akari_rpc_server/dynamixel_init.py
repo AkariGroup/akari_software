c09b15df446db1ff79c4cdee38c9ec91db176576
Yuki Igarashi <me@bonprosoft.com>
2022-12-06 08:52:09 +0900

    Try a given baudrate value first

--- internal/akari_rpc_server/akari_rpc_server/dynamixel_init.py
+++ internal/akari_rpc_server/akari_rpc_server/dynamixel_init.py
@@ -15,8 +15,9 @@ BAUDRATE_GUESSES = [9600, 57600, 115200, 1000000, 2000000, 3000000, 4000000, 450
 
 def initialize_baudrate(config: JointManagerDynamixelSerialConfig) -> None:
     dynamixel_ids = [c.dynamixel_id for c in config.controllers]
+    baudrate_entry = dynamixel_communicator.get_baudrate_control_value(config.baudrate)
 
-    for guess in BAUDRATE_GUESSES:
+    for guess in [config.baudrate] + BAUDRATE_GUESSES:
         try:
             with DynamixelCommunicator.open(baudrate=guess) as comm:
                 control = DynamixelControlTable.TORQUE_ENABLE
@@ -24,9 +25,6 @@ def initialize_baudrate(config: JointManagerDynamixelSerialConfig) -> None:
                     comm.write(id, control.address, control.length, False)
 
                 control = DynamixelControlTable.BAUD_RATE
-                baudrate_entry = dynamixel_communicator.get_baudrate_control_value(
-                    config.baudrate
-                )
                 comm.write(1, control.address, control.length, baudrate_entry)
                 _logger.info(
                     f"Successfuly set baudrate to {dynamixel_communicator.DEFAULT_BAUDRATE}"
