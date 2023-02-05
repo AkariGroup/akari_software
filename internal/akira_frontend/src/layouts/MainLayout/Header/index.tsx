f4aecb5216167696026483e4eebb358ecf95147a
Kazuya Yamamoto <konkoro0503@gmail.com>
2023-02-06 06:27:43 +0900

    Clean up unused button (#173)
    Co-authored-by: Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>

--- internal/akira_frontend/src/layouts/MainLayout/Header/index.tsx
+++ internal/akira_frontend/src/layouts/MainLayout/Header/index.tsx
@@ -1,19 +1,13 @@
 import {
   AppBar as MuiAppBar,
-  Badge,
   Box,
   IconButton,
-  Menu,
-  MenuItem,
   styled,
   Toolbar,
-  Tooltip,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useCallback, useState } from "react";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import { useCallback } from "react";
 import { SidebarWidth } from "../Sidebar";
 import { useSidebarSetValue } from "../../../contexts/SidebarContext";
 
@@ -29,17 +23,7 @@ const AppBar = styled(MuiAppBar)(({ theme }) => ({
 
 export function Header() {
   const setSidebar = useSidebarSetValue();
-  const [anchorElAvatar, setAnchorElAvatar] = useState<HTMLElement | null>(
-    null
-  );
 
-  const handleAvatarClick: React.MouseEventHandler<HTMLButtonElement> =
-    useCallback((e) => {
-      setAnchorElAvatar(e.currentTarget);
-    }, []);
-  const closeAvatarMenu = useCallback(() => {
-    setAnchorElAvatar(null);
-  }, []);
   const toggleSidebar = useCallback(() => {
     setSidebar((prev) => !prev);
   }, [setSidebar]);
@@ -64,41 +48,6 @@ export function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Akari Web Console
           </Typography>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <IconButton size="large" color="inherit">
-              <Badge badgeContent={1} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <Tooltip title="Open user settings">
-              <IconButton
-                size="large"
-                onClick={handleAvatarClick}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              anchorEl={anchorElAvatar}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElAvatar)}
-              onClose={closeAvatarMenu}
-            >
-              <MenuItem onClick={closeAvatarMenu}>Setup SSH key</MenuItem>
-              <MenuItem onClick={closeAvatarMenu}>Log out</MenuItem>
-            </Menu>
-          </Box>
         </Toolbar>
       </AppBar>
     </Box>
