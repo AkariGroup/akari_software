f23dc086403211db1457e8a039fbdac6171a98be
Kazuya Yamamoto <konkoro0503@gmail.com>
2023-04-08 18:32:56 +0900

    Change opencv dependency (#193)
    * Change opencv dependency
    
    * Change opencv dependency
    
    ---------
    
    Co-authored-by: Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>

--- sdk/akari_client/setup.py
+++ sdk/akari_client/setup.py
@@ -36,7 +36,7 @@ setup(
         "depthai": [
             "matplotlib==3.6.2",
             "depthai==2.19.1.0",
-            "opencv-python-headless==4.6.0.66",
+            "opencv-python",
             "blobconverter==1.3.0",
         ],
     },
