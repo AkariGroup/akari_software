8315fa8d80805b85db619066a60c3eaf456fcb1b
Yuki Igarashi <me@bonprosoft.com>
2023-04-16 09:46:54 +0900

    Create akari_projects directory at akira ansible role (#207)
    Create akari-projects directory at akira ansible role

--- setup/ansible/roles/akira/files/daemon.d/env.sh
+++ setup/ansible/roles/akira/files/daemon.d/env.sh
@@ -4,6 +4,6 @@ export HOST_GID=$(id -g)
 export HOST_UID=$(id -u)
 export DOCKER_GID=$(getent group docker | cut -d: -f3)
 export AKIRA_IMAGE_TAG=${AKIRA_IMAGE_TAG:-v1}
-export AKIRA_PROJECT_DIR=${HOME}/projects
+export AKIRA_PROJECT_DIR=${HOME}/akari_projects
 
 exec "$@"
