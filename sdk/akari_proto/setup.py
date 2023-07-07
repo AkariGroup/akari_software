0d3df444c98c15b27b8e9f1dd084507c228f2b98
Kazuya Yamamoto <konkoro0503@gmail.com>
2023-07-07 21:58:53 +0900

    Bump version to 0.3.3 (#222)
    BUmp version to 0.3.3
    
    Co-authored-by: Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>

--- sdk/akari_proto/setup.py
+++ sdk/akari_proto/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="akari-proto",
-    version="0.3.2",
+    version="0.3.3",
     description="Python package of akari protobuf definitions and utility functions",
     packages=find_packages(exclude=["tests"]),
     author="akari",
