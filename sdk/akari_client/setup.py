b9e5fa79efeab7d1fcf2e60ccbaa6ef5c625f924
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-06-17 16:38:55 +0900

    Fix numpy ver

--- sdk/akari_client/setup.py
+++ sdk/akari_client/setup.py
@@ -26,6 +26,7 @@ setup(
     install_requires=[
         "dynamixel_sdk",
         "pydantic>=1.5,<2.0",
+        "numpy==1.26.4",
     ],
     package_data={"akari_client": ["py.typed"]},
     extras_require={
