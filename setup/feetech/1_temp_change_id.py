2de40176b5f4dfbef035add1210659f1f899fa54
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-08-16 23:23:17 +0900

    Add feetech set up script

---
+++ setup/feetech/1_temp_change_id.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import argparse
+from scservo_sdk import *  # Uses SCServo SDK library
+
+protocol_end = 0  # SCServo bit end(STS/SMS=0, SCS=1)
+badurate_list = [1000000, 500000, 250000, 128000, 115200, 76800, 57600, 38400]
+
+
+def main() -> None:
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="シリアルポートを指定します",
+        default="/dev/ttyACA0",
+        type=str,
+    )
+    parser.add_argument(
+        "-c",
+        "--changed_id",
+        help="変更後のfeetechサーボのidを指定します",
+        default=100,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    if os.name == "nt":
+        import msvcrt
+
+    else:
+        import sys, tty, termios
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+
+    portHandler = PortHandler(args.port)
+    packetHandler = PacketHandler(protocol_end)
+
+    # Open port
+    if portHandler.openPort():
+        print("シリアルポートを開きました。")
+    else:
+        print("[ERROR] シリアルポートのOpenに失敗しました。")
+        quit()
+
+    for baudrate in badurate_list:
+        # Set port baudrate
+        if portHandler.setBaudRate(baudrate):
+            print(f"シリアルポートをbaudrate: {baudrate} にセットしました。feetechサーボを探索します。")
+        else:
+            print(f"シリアルポートがbaudrate: {baudrate}に対応していません。スキップします。")
+
+        for id in range(0, args.search_id):
+            scs_model_number, scs_comm_result, scs_error = packetHandler.ping(
+                portHandler, id
+            )
+            if scs_comm_result == COMM_SUCCESS and scs_error == 0:
+                print("------------------------------------------------------------")
+                print(
+                    "Feetechサーボが見つかりました。Baudrate: %d, ID:%03d, モデル名:%d"
+                    % (baudrate, id, scs_model_number)
+                )
+                print()
+                # EEPROM ROCK解除
+                scs_comm_result, scs_error = packetHandler.write1ByteTxRx(
+                    portHandler, id, 55, 0
+                )
+                if scs_comm_result == COMM_SUCCESS:
+                    print(f"EEPROMロック解除しました。")
+                else:
+                    print(f"[ERROR] EPROMロック解除に失敗しました。")
+                    return
+
+                # IDの変更
+                for i in range(0, 3):
+                    time.sleep(0.5)
+                    scs_comm_result, scs_error = packetHandler.write1ByteTxRx(
+                        portHandler, id, 5, args.changed_id
+                    )
+                    if scs_comm_result == COMM_SUCCESS or COMM_RX_TIMEOUT:
+                        print(f"サーボのidを {id} から {args.changed_id} に変更しました。")
+                        break
+                    else:
+                        if i == 2:
+                            print(
+                                "[ERROR] サーボのidの変更に失敗しました。current_idの間違い、モータの接続間違いがないか確認してください。"
+                            )
+                            return
+
+                time.sleep(0.5)
+                print()
+                print("変更したidでpingを試します。")
+
+                # Try to ping the SCServo
+                # Get SCServo model number
+                for i in range(0, 3):
+                    scs_model_number, scs_comm_result, scs_error = packetHandler.ping(
+                        portHandler, args.changed_id
+                    )
+                    if scs_comm_result != COMM_SUCCESS:
+                        if i == 2:
+                            print("[ERROR] %s" % packetHandler.getTxRxResult(scs_comm_result))
+                            print("------------------------")
+                            print("pingに失敗しました")
+                            print("------------------------")
+                            break
+                        continue
+                    elif scs_error != 0:
+                        if i == 2:
+                            print("[ERROR] %s" % packetHandler.getRxPacketError(scs_error))
+                            print("------------------------")
+                            print("pingに失敗しました")
+                            print("------------------------")
+                            break
+                        continue
+                    else:
+                        print(
+                            "Pingに成功しました。 [ID:%03d]. モータモデル : %d"
+                            % (args.changed_id, scs_model_number)
+                        )
+                        # EEPROM ROCK
+                        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(
+                            portHandler, args.changed_id, 55, 1
+                        )
+                        if scs_comm_result == COMM_SUCCESS:
+                            print(f"EEPROMロックしました。")
+                        else:
+                            print(f"[ERROR] EPROMロックに失敗しました。")
+                            return
+                        print("------------------------")
+                        print("idの変更OK!")
+                        print("------------------------")
+                # Close port
+                portHandler.closePort()
+                return
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup/feetech/move_0pos.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+from scservo_sdk import *  # Uses SCServo SDK library
+
+# Control table address
+ADDR_SCS_TORQUE_ENABLE = 40
+ADDR_SCS_GOAL_POSITION = 42
+ADDR_SCS_GOAL_SPEED = 46
+
+# Default setting
+protocol_end = 0  # SCServo bit end(STS/SMS=0, SCS=1)
+motor_velocity = 300
+
+
+def main() -> None:
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="シリアルポートを指定します",
+        default="/dev/ttyACA0",
+        type=str,
+    )
+    parser.add_argument(
+        "-b",
+        "--baudrate",
+        help="Baudrateを指定します",
+        default=500000,
+        type=int,
+    )
+    parser.add_argument(
+        "-m",
+        "--move_pos",
+        help="移動先のfeetechサーボのposを指定します",
+        default=2048,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    if os.name == "nt":
+        import msvcrt
+
+    else:
+        import sys, tty, termios
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+
+    portHandler = PortHandler(args.port)
+    packetHandler = PacketHandler(protocol_end)
+
+    # Open port
+    if portHandler.openPort():
+        print("シリアルポートを開きました。")
+    else:
+        print("[ERROR] シリアルポートのOpenに失敗しました。")
+        quit()
+
+    # Set port baudrate
+    if portHandler.setBaudRate(args.baudrate):
+        print(f"シリアルポートをbaudrate {args.baudrate} にセットしました。")
+    else:
+        print("[ERROR] シリアルポートのbaudrateの変更に失敗しました。")
+        quit()
+
+        cur_position, scs_comm_result, scs_error = packetHandler.read2ByteTxRx(
+            portHandler, id, 56
+        )
+        if scs_comm_result == COMM_SUCCESS:
+            print(f"id: {id}, 現在位置: {cur_position}")
+        else:
+            print("[ERROR] 現在位置の取得に失敗しました。id, Baudrate間違い、モータの接続間違いがないか確認してください。")
+            return
+
+    print()
+    print("ENTERキーを入力すると、全軸サーボONします。")
+    input()
+
+    for id in range(1, 5):
+        # サーボON
+        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(
+            portHandler, id, ADDR_SCS_TORQUE_ENABLE, 1
+        )
+        if scs_comm_result != COMM_SUCCESS:
+            print(f"[ERROR] ID: {id} のサーボONに失敗しました。ID, Baudrateが正しいか、接続が正しいか確認してください。")
+            print("[ERROR] %s" % packetHandler.getTxRxResult(scs_comm_result))
+            return
+        elif scs_error != 0:
+            print(f"[ERROR] ID: {id} のサーボONに失敗しました。ID, Baudrateが正しいか、接続が正しいか確認してください。")
+            print("[ERROR] %s" % packetHandler.getRxPacketError(scs_error))
+            return
+
+    print()
+    print(f"ENTERキーを入力すると、サーボが{args.move_pos}に動きます。")
+    input()
+
+    for id in range(1, 5):
+        # 速度を落とす
+        scs_comm_result, scs_error = packetHandler.write2ByteTxRx(
+            portHandler, id, ADDR_SCS_GOAL_SPEED, motor_velocity
+        )
+        # 0pos
+        scs_comm_result, scs_error = packetHandler.write2ByteTxRx(
+            portHandler, id, ADDR_SCS_GOAL_POSITION, args.move_pos
+        )
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+    print()
+    print("")
+    print("ENTERキーを入力するとサーボOFFします。")
+    input()
+
+    for id in range(1, 5):
+        # サーボOFF
+        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(
+            portHandler, id, ADDR_SCS_TORQUE_ENABLE, 0
+        )
+        if scs_comm_result == COMM_SUCCESS:
+            print(f"id:{id} サーボOFFしました。")
+        else:
+            print("[ERROR] %s" % packetHandler.getTxRxResult(scs_comm_result))
+        if scs_error != 0:
+            print("[ERROR] %s" % packetHandler.getRxPacketError(scs_error))
+
+    print("------------------------")
+    print("動作完了！")
+    print("------------------------")
+    # Close port
+    portHandler.closePort()
+
+
+if __name__ == "__main__":
+    main()
