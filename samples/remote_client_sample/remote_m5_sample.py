7f56d7a64cd49dfff61281a2990ef7f001bf7b21
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-09-30 15:25:43 +0900

    Fix samples

--- samples/remote_client_sample/remote_m5_sample.py
+++ samples/remote_client_sample/remote_m5_sample.py
@@ -25,7 +25,9 @@ def main() -> None:
     メイン関数
     """
     # AKARI本体のIPアドレスを指定する。
-    akari_ip = "192.168.100.58"
+    # 実際のAKARIのIPアドレスに合わせて変更すること。
+    akari_ip = "192.168.100.1"
+    # portは初期設定のままであれば51001固定
     akari_port = "51001"
     akari_endpoint = f"{akari_ip}:{akari_port}"
 
@@ -39,177 +41,184 @@ def main() -> None:
         joint_manager=joint_config, m5stack=m5_config
     )
     # akari_client_configを引数にしてAkariClientを作成する。
-    with AkariClient(akari_client_config) as akari:
-        # m5と通信するクラスを呼び出す
-        m5 = akari.m5stack
-        pos_x: int
-        pos_y: int
-        print("Start!")
+    try:
+        akari = AkariClient(akari_client_config)
+    except Exception as e:
+        print(e)
+        print("")
+        print("接続エラーです。AKARI本体が同一ネットワークに接続されていること、コード内のakari_ipをAKARI本体のIPアドレスに変更してください。")
+        return
+
+    # m5と通信するクラスを呼び出す
+    m5 = akari.m5stack
+    pos_x: int
+    pos_y: int
+    print("Start!")
+    # 2秒停止
+    time.sleep(2)
+    # アプリが終了されるまでループする。
+    while True:
+
+        color = Colors.WHITE
+        m5.set_display_color(color)
+        # STEP1. '1.AKARI'を左上表示
+        print("STEP1. display 1.AKARI at top left")
+        # textは'1.AKARI'
+        text = "1.AKARI"
+        # pos_xは0-320で座標指定。0もしくはPositions.LEFTで左
+        pos_x = Positions.LEFT
+        # pos_yは0-240で座標指定。0もしくはPositions.TOPで上
+        pos_y = Positions.TOP
+        # 文字サイズは5
+        size = 5
+        # 文字色は黒
+        text_color = Colors.BLACK
+        # 背景色は白
+        back_color = Colors.WHITE
+        # 背景全体をリセット
+        refresh = True
+        # set_display_textを実行
+        m5.set_display_text(
+            text, pos_x, pos_y, size, text_color, back_color, refresh
+        )
+        print("-> Set")
         # 2秒停止
         time.sleep(2)
-        # アプリが終了されるまでループする。
-        while True:
+        print()
 
-            color = Colors.WHITE
-            m5.set_display_color(color)
-            # STEP1. '1.AKARI'を左上表示
-            print("STEP1. display 1.AKARI at top left")
-            # textは'1.AKARI'
-            text = "1.AKARI"
-            # pos_xは0-320で座標指定。0もしくはPositions.LEFTで左
-            pos_x = Positions.LEFT
-            # pos_yは0-240で座標指定。0もしくはPositions.TOPで上
-            pos_y = Positions.TOP
-            # 文字サイズは5
-            size = 5
-            # 文字色は黒
-            text_color = Colors.BLACK
-            # 背景色は白
-            back_color = Colors.WHITE
-            # 背景全体をリセット
-            refresh = True
-            # set_display_textを実行
-            m5.set_display_text(
-                text, pos_x, pos_y, size, text_color, back_color, refresh
-            )
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP2. '2.あかり'を中央に表示。位置、サイズ、文字色はデフォルト
+        # 文字色、背景色はSTEP.1と同様、文字サイズは5、背景リセットはTrueとなる。
+        print("STEP2. display 2.あかり at middle center")
+        text = "2.あかり"
+        # set_display_textを実行
+        m5.set_display_text(text)
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP2. '2.あかり'を中央に表示。位置、サイズ、文字色はデフォルト
-            # 文字色、背景色はSTEP.1と同様、文字サイズは5、背景リセットはTrueとなる。
-            print("STEP2. display 2.あかり at middle center")
-            text = "2.あかり"
-            # set_display_textを実行
-            m5.set_display_text(text)
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP3. '3.灯り'を右下揃えに表示
+        print("STEP3. display 3.灯り at bottom right")
+        # textは'3.灯り'
+        text = "3.灯り"
+        # pos_xはPositions.RIGHTで右揃え
+        pos_x = Positions.RIGHT
+        # pos_yはPositions.BOTTOMで下揃え
+        pos_y = Positions.BOTTOM
+        # 文字サイズは7
+        size = 7
+        # 文字色は白
+        text_color = Colors.WHITE
+        # 背景色は緑
+        back_color = Colors.GREEN
+        # 背景全体をリセット
+        refresh = True
+        # set_display_textを実行
+        m5.set_display_text(
+            text, pos_x, pos_y, size, text_color, back_color, refresh
+        )
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP3. '3.灯り'を右下揃えに表示
-            print("STEP3. display 3.灯り at bottom right")
-            # textは'3.灯り'
-            text = "3.灯り"
-            # pos_xはPositions.RIGHTで右揃え
-            pos_x = Positions.RIGHT
-            # pos_yはPositions.BOTTOMで下揃え
-            pos_y = Positions.BOTTOM
-            # 文字サイズは7
-            size = 7
-            # 文字色は白
-            text_color = Colors.WHITE
-            # 背景色は緑
-            back_color = Colors.GREEN
-            # 背景全体をリセット
-            refresh = True
-            # set_display_textを実行
-            m5.set_display_text(
-                text, pos_x, pos_y, size, text_color, back_color, refresh
-            )
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP4. '4.アカリ'を(50,40)に表示
+        print("STEP4. display 4.アカリ at (50,40)")
+        # textは'4.アカリ'
+        text = "4.アカリ"
+        # pos_xは60
+        pos_x = 50
+        # pos_yは40
+        pos_y = 40
+        # 文字サイズは3
+        size = 3
+        # 文字色は青
+        text_color = Colors.BLUE
+        # 背景色は黄
+        back_color = Colors.YELLOW
+        # 背景全体をリセット
+        refresh = True
+        # set_display_textを実行
+        m5.set_display_text(
+            text, pos_x, pos_y, size, text_color, back_color, refresh
+        )
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP4. '4.アカリ'を(50,40)に表示
-            print("STEP4. display 4.アカリ at (50,40)")
-            # textは'4.アカリ'
-            text = "4.アカリ"
-            # pos_xは60
-            pos_x = 50
-            # pos_yは40
-            pos_y = 40
-            # 文字サイズは3
-            size = 3
-            # 文字色は青
-            text_color = Colors.BLUE
-            # 背景色は黄
-            back_color = Colors.YELLOW
-            # 背景全体をリセット
-            refresh = True
-            # set_display_textを実行
-            m5.set_display_text(
-                text, pos_x, pos_y, size, text_color, back_color, refresh
-            )
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP5. '5.アカリ'を(50,100)に背景リフレッシュなしで重ねて表示
+        print("STEP5. display 5.アカリ at (50,100) without background refresh")
+        # textは'5.アカリ'
+        text = "5.アカリ"
+        # pos_xは50
+        pos_x = 50
+        # pos_yは40
+        pos_y = 100
+        # 文字サイズは5
+        size = 5
+        # 文字色は黒
+        text_color = Colors.BLACK
+        # 背景色は紫
+        back_color = Colors.MAGENTA
+        # refreshをFalseにすることで前の背景表示を消さずに追記できる。
+        refresh = False
+        # set_display_textを実行
+        m5.set_display_text(
+            text, pos_x, pos_y, size, text_color, back_color, refresh
+        )
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP5. '5.アカリ'を(50,100)に背景リフレッシュなしで重ねて表示
-            print("STEP5. display 5.アカリ at (50,100) without background refresh")
-            # textは'5.アカリ'
-            text = "5.アカリ"
-            # pos_xは50
-            pos_x = 50
-            # pos_yは40
-            pos_y = 100
-            # 文字サイズは5
-            size = 5
-            # 文字色は黒
-            text_color = Colors.BLACK
-            # 背景色は紫
-            back_color = Colors.MAGENTA
-            # refreshをFalseにすることで前の背景表示を消さずに追記できる。
-            refresh = False
-            # set_display_textを実行
-            m5.set_display_text(
-                text, pos_x, pos_y, size, text_color, back_color, refresh
-            )
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP6. '6.アカリ'を(50,170)にリフレッシュなしで重ねて表示
+        print("STEP6. display 6.アカリ at (50,170) without background refresh")
+        # textは'6.アカリ'
+        text = "6.アカリ"
+        # pos_xは50
+        pos_x = 50
+        # pos_yは170
+        pos_y = 170
+        # 文字サイズは3
+        size = 3
+        # 文字色はピンク
+        text_color = Colors.PINK
+        # 背景色は灰
+        back_color = Colors.DARKGREY
+        # refreshをFalseにすることで前の表示を消さずに追記できる。
+        refresh = False
+        # set_display_textを実行
+        m5.set_display_text(
+            text, pos_x, pos_y, size, text_color, back_color, refresh
+        )
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP6. '6.アカリ'を(50,170)にリフレッシュなしで重ねて表示
-            print("STEP6. display 6.アカリ at (50,170) without background refresh")
-            # textは'6.アカリ'
-            text = "6.アカリ"
-            # pos_xは50
-            pos_x = 50
-            # pos_yは170
-            pos_y = 170
-            # 文字サイズは3
-            size = 3
-            # 文字色はピンク
-            text_color = Colors.PINK
-            # 背景色は灰
-            back_color = Colors.DARKGREY
-            # refreshをFalseにすることで前の表示を消さずに追記できる。
-            refresh = False
-            # set_display_textを実行
-            m5.set_display_text(
-                text, pos_x, pos_y, size, text_color, back_color, refresh
-            )
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
+        # STEP7. '7.灯'を中央に表示
+        print("STEP7. display 7.灯 at middle center")
+        # textは'7.灯'
+        text = "7.灯"
+        # 文字サイズは11
+        size = 11
+        # 文字色は白
+        text_color = Colors.WHITE
+        # 背景色は黒
+        back_color = Colors.BLACK
+        # set_display_textを実行
+        # pos_x,pos_yのみデフォルト引数を使用
+        m5.set_display_text(text, size=size)
+        print("-> Set")
+        # 2秒停止
+        time.sleep(2)
+        print()
 
-            # STEP7. '7.灯'を中央に表示
-            print("STEP7. display 7.灯 at middle center")
-            # textは'7.灯'
-            text = "7.灯"
-            # 文字サイズは11
-            size = 11
-            # 文字色は白
-            text_color = Colors.WHITE
-            # 背景色は黒
-            back_color = Colors.BLACK
-            # set_display_textを実行
-            # pos_x,pos_yのみデフォルト引数を使用
-            m5.set_display_text(text, size=size)
-            print("-> Set")
-            # 2秒停止
-            time.sleep(2)
-            print()
-
-            print("Finish!")
-            print()
-            # 5秒停止
-            time.sleep(5)
+        print("Finish!")
+        print()
+        # 5秒停止
+        time.sleep(5)
 
 
 if __name__ == "__main__":
--- samples/remote_client_sample/remote_motor_sample.py
+++ samples/remote_client_sample/remote_motor_sample.py
@@ -10,125 +10,137 @@ from akari_client.config import (
 )
 
 
-# AKARI本体のIPアドレスを指定する。
-akari_ip = "192.168.100.58"
-akari_port = "51001"
-akari_endpoint = f"{akari_ip}:{akari_port}"
+def main() -> None:
+    """
+    メイン関数
+    """
+    # AKARI本体のIPアドレスを指定する。
+    # 実際のAKARIのIPアドレスに合わせて変更すること。
+    akari_ip = "192.168.100.1"
+    # portは初期設定のままであれば51001固定
+    akari_port = "51001"
+    akari_endpoint = f"{akari_ip}:{akari_port}"
 
-joint_config: JointManagerGrpcConfig = JointManagerGrpcConfig(
-    type="grpc", endpoint=akari_endpoint
-)
-m5_config: M5StackGrpcConfig = M5StackGrpcConfig(type="grpc", endpoint=akari_endpoint)
-akari_client_config = AkariClientConfig(joint_manager=joint_config, m5stack=m5_config)
-# akari_client_configを引数にしてAkariClientを作成する。
-akari = AkariClient(akari_client_config)
+    joint_config: JointManagerGrpcConfig = JointManagerGrpcConfig(
+        type="grpc", endpoint=akari_endpoint
+    )
+    m5_config: M5StackGrpcConfig = M5StackGrpcConfig(type="grpc", endpoint=akari_endpoint)
+    akari_client_config = AkariClientConfig(joint_manager=joint_config, m5stack=m5_config)
+    # akari_client_configを引数にしてAkariClientを作成する。
+    try:
+        akari = AkariClient(akari_client_config)
+    except Exception as e:
+        print(e)
+        print("")
+        print("接続エラーです。AKARI本体が同一ネットワークに接続されていること、コード内のakari_ipをAKARI本体のIPアドレスに変更してください。")
+        return
 
-# 処理を記載。下記は例
-joints = akari.joints
-# STEP1.各軸の名前を取得し、表示する。
-print("STEP1. Get joint names")
-print(joints.get_joint_names())
-print("")
-time.sleep(3)
+    # 処理を記載。下記は例
+    joints = akari.joints
+    # STEP1.各軸の名前を取得し、表示する。
+    print("STEP1. Get joint names")
+    print(joints.get_joint_names())
+    print("")
+    time.sleep(3)
 
-# STEP2.サーボトルクをONする。
-print("STEP2. Servo torque ON")
-joints.enable_all_servo()
-print("")
-time.sleep(3)
+    # STEP2.サーボトルクをONする。
+    print("STEP2. Servo torque ON")
+    joints.enable_all_servo()
+    print("")
+    time.sleep(3)
 
-# STEP3.初期位置に移動する。"pan","tilt"それぞれの位置を0にしている。syncをTrueにすることで、移動完了まで待つ
-print("STEP3. Move to initial position. Wait until finish.")
-joints.move_joint_positions(sync=True, pan=0, tilt=0)
-print("")
+    # STEP3.初期位置に移動する。"pan","tilt"それぞれの位置を0にしている。syncをTrueにすることで、移動完了まで待つ
+    print("STEP3. Move to initial position. Wait until finish.")
+    joints.move_joint_positions(sync=True, pan=0, tilt=0)
+    print("")
 
-# STEP4.現在の軸の位置を表示。コマンドラインに表示される。
-print("STEP4. Get current joint position [rad]")
-print(joints.get_joint_positions())
-print("")
-time.sleep(3)
+    # STEP4.現在の軸の位置を表示。コマンドラインに表示される。
+    print("STEP4. Get current joint position [rad]")
+    print(joints.get_joint_positions())
+    print("")
+    time.sleep(3)
 
-# STEP5.panのモータだけ速度を3rad/sに変更する。
-print("STEP5. Set pan joint velocity at 3 rad/s")
-joints.set_joint_velocities(pan=3)
-print("")
-time.sleep(3)
+    # STEP5.panのモータだけ速度を3rad/sに変更する。
+    print("STEP5. Set pan joint velocity at 3 rad/s")
+    joints.set_joint_velocities(pan=3)
+    print("")
+    time.sleep(3)
 
-# STEP6.panのモータ位置だけを0.5[rad]に移動。
-print("STEP6. Move pan joint position to 0.5 rad")
-joints.move_joint_positions(sync=True, pan=0.5)
-print("")
-time.sleep(3)
+    # STEP6.panのモータ位置だけを0.5[rad]に移動。
+    print("STEP6. Move pan joint position to 0.5 rad")
+    joints.move_joint_positions(sync=True, pan=0.5)
+    print("")
+    time.sleep(3)
 
-# STEP7.tiltのモータだけ速度を6rad/sに変更する。
-print("STEP7. Set tilt joint velocity at 6 rad/s")
-joints.set_joint_velocities(tilt=6)
-print("")
-time.sleep(3)
+    # STEP7.tiltのモータだけ速度を6rad/sに変更する。
+    print("STEP7. Set tilt joint velocity at 6 rad/s")
+    joints.set_joint_velocities(tilt=6)
+    print("")
+    time.sleep(3)
 
-# STEP8.tiltのモータ位置だけを0.5[rad]に移動。
-print("STEP8. Move tilt joint position to 0.5 rad")
-joints.move_joint_positions(tilt=0.5)
-print("")
-time.sleep(3)
+    # STEP8.tiltのモータ位置だけを0.5[rad]に移動。
+    print("STEP8. Move tilt joint position to 0.5 rad")
+    joints.move_joint_positions(tilt=0.5)
+    print("")
+    time.sleep(3)
 
-# STEP9.tiltのモータだけ加速度を1rad/sに変更する。
-print("STEP9. Set tilt joint accerelation at 1 rad/s")
-joints.set_joint_accelerations(tilt=1)
-print("")
-time.sleep(3)
+    # STEP9.tiltのモータだけ加速度を1rad/sに変更する。
+    print("STEP9. Set tilt joint accerelation at 1 rad/s")
+    joints.set_joint_accelerations(tilt=1)
+    print("")
+    time.sleep(3)
 
-# STEP10.tiltのモータ位置だけを-0.4[rad]に移動。
-# syncをTrueにしていないため、移動完了前に次のコマンドが実行される。
-print("STEP10. Move tilt joint position to -0.4 rad")
-joints.move_joint_positions(tilt=-0.4)
-print("")
-time.sleep(0.5)
+    # STEP10.tiltのモータ位置だけを-0.4[rad]に移動。
+    # syncをTrueにしていないため、移動完了前に次のコマンドが実行される。
+    print("STEP10. Move tilt joint position to -0.4 rad")
+    joints.move_joint_positions(tilt=-0.4)
+    print("")
+    time.sleep(0.5)
 
-# STEP11.panのモータ位置だけを-0.3[rad]に移動。
-# syncをTrueにしていないため、移動完了前に次のコマンドが実行される。
-print("STEP10. Move pan joint position to -0.3 rad")
-joints.move_joint_positions(pan=-0.3)
-print("")
-time.sleep(0.5)
+    # STEP11.panのモータ位置だけを-0.3[rad]に移動。
+    # syncをTrueにしていないため、移動完了前に次のコマンドが実行される。
+    print("STEP10. Move pan joint position to -0.3 rad")
+    joints.move_joint_positions(pan=-0.3)
+    print("")
+    time.sleep(0.5)
 
-# STEP12.pan,tiltのモータ位置を(0.4, 0.4)[rad]に移動。syncをTrueにすることで、移動完了まで待つ
-print(
-    "STEP12. Move pan joint position to 0.4 rad and tilt joint position to 0.4 rad. Wait until finish."
-)
-joints.move_joint_positions(sync=True, pan=0.4, tilt=0.4)
-print("")
+    # STEP12.pan,tiltのモータ位置を(0.4, 0.4)[rad]に移動。syncをTrueにすることで、移動完了まで待つ
+    print(
+        "STEP12. Move pan joint position to 0.4 rad and tilt joint position to 0.4 rad. Wait until finish."
+    )
+    joints.move_joint_positions(sync=True, pan=0.4, tilt=0.4)
+    print("")
 
-# STEP11.両方のモータ速度を10rad/sに変更。
-print("STEP11. Set both joints velocity at 10 rad/s")
-joints.set_joint_velocities(pan=10, tilt=10)
-print("")
-time.sleep(3)
+    # STEP11.両方のモータ速度を10rad/sに変更。
+    print("STEP11. Set both joints velocity at 10 rad/s")
+    joints.set_joint_velocities(pan=10, tilt=10)
+    print("")
+    time.sleep(3)
 
-# STEP12.両方のモータ加速度を10rad/sに変更する。
-print("STEP12. Set both joints accerelation at 10 rad/s")
-joints.set_joint_accelerations(pan=10, tilt=10)
-print("")
-time.sleep(3)
+    # STEP12.両方のモータ加速度を10rad/sに変更する。
+    print("STEP12. Set both joints accerelation at 10 rad/s")
+    joints.set_joint_accelerations(pan=10, tilt=10)
+    print("")
+    time.sleep(3)
 
-# STEP13.パンのモータ位置を-0.7,チルトのモータ位置を-0.3に移動。
-print("STEP13. Move pan joint position to -0.7 rad and tilt joint position to -0.3 rad")
-joints.move_joint_positions(pan=-0.7, tilt=-0.3)
-print("")
-time.sleep(3)
+    # STEP13.パンのモータ位置を-0.7,チルトのモータ位置を-0.3に移動。
+    print("STEP13. Move pan joint position to -0.7 rad and tilt joint position to -0.3 rad")
+    joints.move_joint_positions(pan=-0.7, tilt=-0.3)
+    print("")
+    time.sleep(3)
 
-# STEP14.現在の軸の位置を表示。コマンドラインに表示される。
-# [-0.7, 0.3]とほぼ一致していれば成功
-print("STEP14. Get current joint position [rad]")
-print(joints.get_joint_positions())
-print("")
-time.sleep(3)
+    # STEP14.現在の軸の位置を表示。コマンドラインに表示される。
+    # [-0.7, 0.3]とほぼ一致していれば成功
+    print("STEP14. Get current joint position [rad]")
+    print(joints.get_joint_positions())
+    print("")
+    time.sleep(3)
 
-# STEP15.初期位置に戻る。
-print("STEP15. Return to initial position")
-joints.move_joint_positions(pan=0, tilt=0)
-print("")
-time.sleep(3)
+    # STEP15.初期位置に戻る。
+    print("STEP15. Return to initial position")
+    joints.move_joint_positions(pan=0, tilt=0)
+    print("")
+    time.sleep(3)
 
 
 if __name__ == "__main__":
