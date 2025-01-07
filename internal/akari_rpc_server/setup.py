e0b25f3ebfa60df248ac6e3f6bf9a9d0d23852b8
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2025-01-07 13:32:45 +0900

    Update rpc server

--- internal/akari_rpc_server/setup.py
+++ internal/akari_rpc_server/setup.py
@@ -8,8 +8,8 @@ setup(
     author_email="akari.tmc@gmail.com",
     package_data={"akari_rpc_server": ["py.typed"]},
     install_requires=[
-        "akari-proto>=0.3.0,<0.4.0",
-        "grpcio==1.60.0",
-        "protobuf==4.25.3",
+        "akari-proto>=0.4.0,<0.5.0",
+        "grpcio==1.68.1",
+        "protobuf==5.29.2",
     ],
 )
