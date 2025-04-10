b9bc0d6885c6288cfc3bf095e18c7b65816d26f3
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2025-04-10 16:30:38 +0900

    Fix format

--- internal/akari_rpc_server/akari_rpc_server/server.py
+++ internal/akari_rpc_server/akari_rpc_server/server.py
@@ -22,8 +22,10 @@ from .m5stack import M5StackServiceServicer
 def serve(port: int) -> None:
     with contextlib.ExitStack() as stack:
         config = load_config()
-        
-        if not isinstance(config.joint_manager, JointManagerDynamixelSerialConfig) and not isinstance(config.joint_manager, JointManagerFeetechSerialConfig):
+
+        if not isinstance(
+            config.joint_manager, JointManagerDynamixelSerialConfig
+        ) and not isinstance(config.joint_manager, JointManagerFeetechSerialConfig):
             logging.warning("Invalid JointManager configuration")
         if not isinstance(config.m5stack, M5StackSerialConfig):
             logging.warning("Invalid M5Stack configuration")
@@ -31,12 +33,12 @@ def serve(port: int) -> None:
         client: AkariClient = stack.enter_context(AkariClient(config))
         joint_manager = None
         m5stack = None
-        
+
         try:
             joint_manager = client.joints
         except RuntimeError as e:
             logging.warning(f"Servo motor initialization error: {e}")
-            
+
         try:
             m5stack = client.m5stack
             if not isinstance(m5stack, M5StackSerialClient):
@@ -46,19 +48,23 @@ def serve(port: int) -> None:
             logging.warning(f"M5Stack initialization error: {e}")
 
         server = grpc.server(ThreadPoolExecutor(max_workers=10))
-        
+
         if joint_manager is not None:
             if isinstance(config.joint_manager, JointManagerDynamixelSerialConfig):
                 joints_controller_pb2_grpc.add_JointsControllerServiceServicer_to_server(
-                    DynamixelControllerServiceServicer(config.joint_manager, joint_manager),
+                    DynamixelControllerServiceServicer(
+                        config.joint_manager, joint_manager
+                    ),
                     server,
                 )
             elif isinstance(config.joint_manager, JointManagerFeetechSerialConfig):
                 joints_controller_pb2_grpc.add_JointsControllerServiceServicer_to_server(
-                    FeetechControllerServiceServicer(config.joint_manager, joint_manager),
+                    FeetechControllerServiceServicer(
+                        config.joint_manager, joint_manager
+                    ),
                     server,
                 )
-        
+
         if m5stack is not None and isinstance(m5stack, M5StackSerialClient):
             m5stack_pb2_grpc.add_M5StackServiceServicer_to_server(
                 M5StackServiceServicer(m5stack),
